c37a71214638e1cb394006adf4023dc54ef31f9a
Amanda Ventura <saenz242ever@gmail.com>
2024-09-09 19:32:05 -0600

    added dictonary js and css and added search engine js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <div className="container">
+        <header className="App-header text-center">
+          Dictionary
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="app-footer">
+          <small>
+            Coded by Amanda Ventura
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
