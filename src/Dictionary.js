7b440315e60ef0c0b2a851d56854641a6fc9b166
Amanda Ventura <saenz242ever@gmail.com>
2024-09-10 01:01:09 -0600

    added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,20 @@
 import React, {useState} from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("");
+
+    function handleResponse(response) {
+        console.log(response.data.meanings[0]);
+
+    }
+
     function search(event){
         event.preventDefault();
-        alert(`Searching for ${keyword}`);
+        
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=6a8co22f6f92bdd5a654001ta38ff409`;
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function handleKeywordChange(event){
