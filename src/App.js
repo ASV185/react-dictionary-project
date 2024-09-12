f4f1b158bc5429ab8b7406806a195fa9265bd599
Amanda Ventura <saenz242ever@gmail.com>
2024-09-12 00:14:16 -0600

    added styling to headings

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <div className="container">
         <header className="App-header text-center">
-          <h1>Dictionary</h1>
+          <h1 className="m-5">Dictionary</h1>
         </header>
         <main>
           <Dictionary />
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import "./Meaning.css";
 
 export default function Meaning(props){
     console.log(props.meaning);
     return (
         <div className="Meaning">
             <h3>{props.meaning.partOfSpeech}</h3>
-            <p><strong>Definition: </strong>{props.meaning.definition}</p>
-            <p><strong>Example: </strong><em>{props.meaning.example}</em></p>
+            <p><strong>Definition: </strong>{props.meaning.definition}.</p>
+            <p><strong>Example: </strong><em>{props.meaning.example}.</em></p>
             <Synonyms synonyms = {props.meaning.synonyms}/>
         </div>
     );
