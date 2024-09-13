71162540ee68ac416d7fceec5f5856da3f747150
Amanda Ventura <saenz242ever@gmail.com>
2024-09-13 15:15:59 -0600

    made changes to styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-      <div className="container">
+      <div className="m-0 p-0">
         <header className="App-header text-center">
           <h1 className="m-5">Dictionary</h1>
         </header>
--- src/Meaning.js
+++ src/Meaning.js
@@ -5,10 +5,10 @@ import "./Meaning.css";
 export default function Meaning(props){
         return (
             <div className="Meaning">
-              <h3>{props.meaning.partOfSpeech}</h3>
-              <p><strong>Definition: </strong>{props.meaning.definition}.</p>
-              <p><strong>Example: </strong><em>{props.meaning.example}.</em></p>
-              <Synonyms synonyms = {props.meaning.synonyms}/>
-          </div>
+                <h3>{props.meaning.partOfSpeech}</h3>
+                <p><strong>Definition: </strong>{props.meaning.definition[0]}.</p>
+                <p><strong>Ex: </strong><em>{props.meaning.example}</em></p>
+                <Synonyms synonyms = {props.meaning.synonyms}/>
+            </div>
       );
 }
