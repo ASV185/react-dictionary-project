d43b34398b8054fc1da1ea64c499174adb5158d5
Amanda Ventura <saenz242ever@gmail.com>
2024-09-13 15:29:13 -0600

    made correction in meaning component

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,7 +6,7 @@ export default function Meaning(props){
         return (
             <div className="Meaning">
                 <h3>{props.meaning.partOfSpeech}</h3>
-                <p><strong>Definition: </strong>{props.meaning.definition[0]}.</p>
+                <p><strong>Definition: </strong>{props.meaning.definition}.</p>
                 <p><strong>Ex: </strong><em>{props.meaning.example}</em></p>
                 <Synonyms synonyms = {props.meaning.synonyms}/>
             </div>
